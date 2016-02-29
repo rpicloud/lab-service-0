fbd31fb97088579c1e37387127132a6c870cf064
Kasper Nissen <kni@it-minds.dk>
2016-02-29 16:10:03 +0100

    crashing endpoint

--- src/main/java/com/rpicloud/Application.java
+++ src/main/java/com/rpicloud/Application.java
@@ -2,11 +2,27 @@ package com.rpicloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+	private Boolean crash = false;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@RequestMapping("/crash")
+	public String crash(){
+		if(crash){
+			throw new NullPointerException();
+		}
+		crash=true;
+		return "I'm gonna crash next time you call me!";
+	}
+
+
 }
