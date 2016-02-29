f94998ceb369c50f034c64648ac8994309ba2a4b
Kasper Nissen <kni@it-minds.dk>
2016-02-29 15:21:10 +0100

    Initial commit

---
+++ src/main/java/com/rpicloud/Service0Application.java
@@ -0,0 +1,12 @@
+package com.rpicloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service0Application.class, args);
+	}
+}
