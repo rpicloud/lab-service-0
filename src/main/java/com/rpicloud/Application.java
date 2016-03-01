f51547f5d86c984987df3c37e3470ec3aba15cd9
Martin Jensen <martin2610@gmail.com>
2016-03-01 10:39:50 +0100

    Feign and Jackson added. Service1/resource added.

--- src/main/java/com/rpicloud/Application.java
+++ src/main/java/com/rpicloud/Application.java
@@ -1,8 +1,17 @@
 package com.rpicloud;
 
+import com.netflix.ribbon.proxy.annotation.Http;
+import com.rpicloud.models.Resource1;
+import feign.Feign;
+import feign.RequestLine;
+import feign.codec.Decoder;
+import feign.codec.ErrorDecoder;
+import feign.jackson.JacksonDecoder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.integration.IntegrationAutoConfiguration;
+import org.springframework.cloud.netflix.feign.support.SpringDecoder;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +27,8 @@ public class Application {
 
 	private Boolean crash = false;
 	private Integer sleep = 0;
+	private String service1host;
+	private String service1port;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -61,4 +72,39 @@ public class Application {
 	}
 
 
+	// Testing out Feign
+	//Interface without parameters
+	interface Service1 {
+		@RequestLine("GET /resources")
+		List<Resource1> resources();
+	}
+
+	@RequestMapping(value = "/request1")
+	public ResponseEntity<List<Resource1>> request1() {
+
+		Service1 service1 = Feign.builder()
+				.decoder(new JacksonDecoder())
+//				.target(Service1.class, "http://"+service1host+":"+service1port); TODO: Add config
+				.target(Service1.class, "http://localhost:9001");
+
+		// Fetch and print a list of the contributors to this library.
+		List<Resource1> resources = service1.resources();
+
+
+//		List<String> result = new ArrayList<>();
+
+		// Mapping to another type here (just string for now)
+//		for (Resource1 res : resources) {
+//			result.add(res.getData());
+//		}
+
+
+		return new ResponseEntity<List<Resource1>>(resources, HttpStatus.OK);
+	}
+
+	@Autowired
+	void setEnvironment(Environment env){
+		service1host = env.getProperty("configuration.service1.host");
+		service1port = env.getProperty("configuration.service1.port");
+	}
 }
---
+++ src/main/java/com/rpicloud/models/Resource.java
@@ -0,0 +1,8 @@
+package com.rpicloud.models;
+
+/**
+ * Created by mixmox on 01/03/16.
+ */
+public class Resource {
+    String data;
+}
